434da5d6340e7d1b5eabf362611daed4a236d4c1
Ngo Nam <namngo102003@gmail.com>
2024-04-23 23:49:21 +0700

    set up toast message

--- Mobile/CoffeeApp/App.js
+++ Mobile/CoffeeApp/App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Navigation from './navigation/Navigation';
 import { PaperProvider } from 'react-native-paper';
+import Toast from 'react-native-toast-message';
 
 export default function App() {
   return (
     <PaperProvider>
       <Navigation />
+      <Toast />
     </PaperProvider>
   );
 }
