265f8808bcc8c5342c5f9bee1b718c04d6ea7b5e
Ngo Nam <namngo102003@gmail.com>
2024-04-23 23:25:09 +0700

    custom hook

---
+++ Mobile/CoffeeApp/customHooks/getDefaultAddress.js
@@ -0,0 +1,42 @@
+import { View, Text } from 'react-native'
+import React, { useCallback, useEffect, useState } from 'react'
+import { getAddress } from '../controller/AddressController'
+import { useFocusEffect } from '@react-navigation/native'
+
+const getDefaultAddress = () => {
+    const [addressData, setAddressData] = useState(null)
+
+    const handleGetAddresses = async () => {
+        try {
+            const addresses = await getAddress();
+            if (addresses) {
+                let addressDefault = null;
+                for (const address in addresses) {
+                    if (addresses[address].Default) {
+                        addressDefault = addresses[address];
+                        break;
+                    }
+                }
+                if (addressDefault) {
+                    setAddressData(addressDefault);
+                }
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    useFocusEffect(
+        useCallback(() => {
+            handleGetAddresses();
+        }, [])
+    )
+
+    useEffect(() => {
+        handleGetAddresses();
+    }, [])
+
+    return addressData
+}
+
+export default getDefaultAddress
