82323e1e393e5a81ecc1dbf5cf10120f36e40243
Ngo Nam <namngo102003@gmail.com>
2024-04-26 01:02:54 +0700

    filter products are out of stock

--- Mobile/CoffeeApp/screens/HomeScreen.js
+++ Mobile/CoffeeApp/screens/HomeScreen.js
@@ -1,10 +1,20 @@
 import { useNavigation } from "@react-navigation/native";
 import React, { useEffect, useState } from "react";
-import { Image, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+    Image,
+    ScrollView,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+} from "react-native";
 import Draggable from "react-native-draggable";
 import * as Icons from "react-native-heroicons/outline";
 import Carousel from "react-native-reanimated-carousel";
-import { heightPercentageToDP as hp, widthPercentageToDP as wp } from "react-native-responsive-screen";
+import {
+    heightPercentageToDP as hp,
+    widthPercentageToDP as wp,
+} from "react-native-responsive-screen";
 import Categories from "../components/categories";
 import Item from "../components/item";
 import getDefaultAddress from "../customHooks/getDefaultAddress";
@@ -32,40 +42,60 @@ const HomeScreen = () => {
                     SoLuong: listProducts[key].SoLuong,
                     Size: {
                         Nho: {
-                            TenKichThuoc: listProducts[key]?.ChiTietKichThuocSanPham?.KT0001?.TenKichThuoc,
-                            Gia: listProducts[key]?.ChiTietKichThuocSanPham?.KT0001?.Gia
+                            TenKichThuoc:
+                                listProducts[key]?.ChiTietKichThuocSanPham
+                                    ?.KT0001?.TenKichThuoc,
+                            Gia: listProducts[key]?.ChiTietKichThuocSanPham
+                                ?.KT0001?.Gia,
                         },
                         Thuong: {
-                            TenKichThuoc: listProducts[key]?.ChiTietKichThuocSanPham?.KT0002?.TenKichThuoc,
-                            Gia: listProducts[key]?.ChiTietKichThuocSanPham?.KT0002?.Gia
+                            TenKichThuoc:
+                                listProducts[key]?.ChiTietKichThuocSanPham
+                                    ?.KT0002?.TenKichThuoc,
+                            Gia: listProducts[key]?.ChiTietKichThuocSanPham
+                                ?.KT0002?.Gia,
                         },
                         Lon: {
-                            TenKichThuoc: listProducts[key]?.ChiTietKichThuocSanPham?.KT0003?.TenKichThuoc,
-                            Gia: listProducts[key]?.ChiTietKichThuocSanPham?.KT0003?.Gia
+                            TenKichThuoc:
+                                listProducts[key]?.ChiTietKichThuocSanPham
+                                    ?.KT0003?.TenKichThuoc,
+                            Gia: listProducts[key]?.ChiTietKichThuocSanPham
+                                ?.KT0003?.Gia,
                         },
-                    }
-                }
+                    },
+                };
                 allProducts.push(productData);
             }
         }
 
-        setProducts([...allProducts])
-    }
-    
+        setProducts([...allProducts]);
+    };
+
     useEffect(() => {
         handleGetProducts();
-    }, [])
+    }, []);
 
     return (
         <View className="flex-1">
             <ScrollView>
-                <View style={{ height: hp(35) }} className="bg-yellow-950 space-y-5">
+                <View
+                    style={{ height: hp(35) }}
+                    className="bg-yellow-950 space-y-5"
+                >
                     {/* Header */}
-                    <View style={{ width: wp(90) }} className="flex-row justify-between mx-auto mt-20 items-center">
+                    <View
+                        style={{ width: wp(90) }}
+                        className="flex-row justify-between mx-auto mt-20 items-center"
+                    >
                         <View>
                             <Text className="text-white">Giao đến</Text>
-                            <TouchableOpacity onPress={() => navigation.navigate("MapView")}>
-                                <Text className="text-white text-base font-semibold" style={{ width: wp(70) }}>
+                            <TouchableOpacity
+                                onPress={() => navigation.navigate("MapView")}
+                            >
+                                <Text
+                                    className="text-white text-base font-semibold"
+                                    style={{ width: wp(70) }}
+                                >
                                     {addressData?.DiaChi}
                                 </Text>
                             </TouchableOpacity>
@@ -81,10 +111,19 @@ const HomeScreen = () => {
                     {/* Search bar */}
                     <View className="mx-5 py-2 px-3 bg-white rounded-lg">
                         <View className="flex-row justify-between">
-                            <TextInput placeholder="tìm món..." className="text-lg" />
+                            <TextInput
+                                placeholder="tìm món..."
+                                className="text-lg"
+                            />
 
-                            <TouchableOpacity onPress={handleGetProducts} className="p-1 bg-yellow-950 rounded-lg">
-                                <Icons.MagnifyingGlassIcon size={24} color="#ffffff" />
+                            <TouchableOpacity
+                                onPress={handleGetProducts}
+                                className="p-1 bg-yellow-950 rounded-lg"
+                            >
+                                <Icons.MagnifyingGlassIcon
+                                    size={24}
+                                    color="#ffffff"
+                                />
                             </TouchableOpacity>
                         </View>
                     </View>
@@ -98,10 +137,22 @@ const HomeScreen = () => {
                         height={hp(22)}
                         autoPlay={true}
                         data={[
-                            { id: 1, image: require("../assets/images/Frame 17.png") },
-                            { id: 2, image: require("../assets/images/Frame 17.png") },
-                            { id: 3, image: require("../assets/images/Frame 17.png") },
-                            { id: 4, image: require("../assets/images/Frame 17.png") },
+                            {
+                                id: 1,
+                                image: require("../assets/images/Frame 17.png"),
+                            },
+                            {
+                                id: 2,
+                                image: require("../assets/images/Frame 17.png"),
+                            },
+                            {
+                                id: 3,
+                                image: require("../assets/images/Frame 17.png"),
+                            },
+                            {
+                                id: 4,
+                                image: require("../assets/images/Frame 17.png"),
+                            },
                         ]}
                         scrollAnimationDuration={1000}
                         renderItem={({ item }) => (
@@ -123,23 +174,92 @@ const HomeScreen = () => {
 
                 {/* card item */}
                 <View className="mx-5 mt-5 flex-row flex-wrap justify-between">
-                    {products && products.map((product, index)=> {
-                        return (
-                            <Item product={product} key={product.MaSanPham}/>
-                        )
-                    })}
+                    {products &&
+                        products
+                            .filter((item) => item.SoLuong > 0)
+                            .map((product, index) => {
+                                return (
+                                    <Item
+                                        product={product}
+                                        key={product.MaSanPham}
+                                    />
+                                );
+                            })}
                 </View>
+
+                {/* out of stock */}
+                {products &&
+                    products.filter((item) => item.SoLuong == 0).length > 0 && (
+                        <View>
+                            {/* <Text className="text-center text-lg font-bold mt-5">Hết hàng</Text> */}
+                            <View
+                                className="mt-5 flex-row justify-between items-center mx-5"
+                            >
+                                <Text
+                                    style={{
+                                        height: 1,
+                                        borderColor: "rgba(59, 29, 12, 0.4)",
+                                        borderWidth: 1,
+                                        width: wp(30),
+                                    }}
+                                ></Text>
+
+                                <Text style={{ color: "#8B6122" }} className='text-lg font-semibold'>
+                                    Hết hàng
+                                </Text>
+
+                                <Text
+                                    style={{
+                                        height: 1,
+                                        borderColor: "rgba(59, 29, 12, 0.4)",
+                                        borderWidth: 1,
+                                        width: wp(30),
+                                    }}
+                                ></Text>
+                            </View>
+
+                            <View className="mx-5 mt-5 flex-row flex-wrap justify-between">
+                                {products
+                                    .filter((item) => item.SoLuong == 0)
+                                    .map((product, index) => {
+                                        return (
+                                            <Item
+                                                product={product}
+                                                key={product.MaSanPham}
+                                            />
+                                        );
+                                    })}
+                            </View>
+                        </View>
+                    )}
             </ScrollView>
 
             {/* cart */}
-            <Draggable x={wp(80)} y={hp(82)} renderSize={24} renderColor="amber" isCircle>
+            <Draggable
+                x={wp(80)}
+                y={hp(82)}
+                minX={wp(5)}
+                maxX={wp(95)}
+                minY={hp(5)}
+                maxY={hp(90)}
+                renderSize={24}
+                renderColor="amber"
+                isCircle
+            >
                 <View>
                     <TouchableOpacity
                         onPress={() => navigation.navigate("Cart")}
-                        className="p-5 bg-yellow-600 rounded-full">
-                        <Icons.ShoppingCartIcon size={30} strokeWidth={2} color={colors.primary} />
+                        className="p-5 bg-yellow-600 rounded-full"
+                    >
+                        <Icons.ShoppingCartIcon
+                            size={30}
+                            strokeWidth={2}
+                            color={colors.primary}
+                        />
                         <View className="absolute -right-1 top-1 bg-red-500 px-2 rounded-full">
-                            <Text className="text-white text-base">{quantity}</Text>
+                            <Text className="text-white text-base">
+                                {quantity}
+                            </Text>
                         </View>
                     </TouchableOpacity>
                 </View>
