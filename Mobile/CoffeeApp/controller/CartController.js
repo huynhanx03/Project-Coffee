98081e91cba0afa07f494b9e7b1737cc3c1d4e7b
Ngo Nam <namngo102003@gmail.com>
2024-04-26 15:31:27 +0700

    logic cart controller

---
+++ Mobile/CoffeeApp/controller/CartController.js
@@ -0,0 +1,79 @@
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import { getDatabase, ref, remove, set, child, } from "firebase/database"
+import getUserData from "./StorageController"
+
+
+const getNewId = async () => {
+    const dbRef = ref(getDatabase())
+    const userData = await getUserData()
+    try {
+        const productSnapshot = await get(child(dbRef, `GioHang/${userData.MaNguoiDung}`))
+        const products = productSnapshot.val()
+
+        if (products) {
+            const currentId = parseInt(Object.keys(products)[Object.keys(products).length - 1].slice(2))
+
+            const newId = 'SP' + String(currentId + 1).padStart(4, '0')
+            return newId
+        } else {
+            return 'SP0001'
+        }
+    } catch (err) {
+        console.log(err)
+    }
+
+}
+
+/**
+ * Set item to cart in database
+ */
+const setCart = async (item) => {
+    const userData = await getUserData()
+    try {
+        const db = getDatabase()
+        set(ref(db, `GioHang/${userData.MaNguoiDung}/${item.MaSanPham}`), {
+            Gia: item.initialPrice,
+            HinhAnh: item.HinhAnh,
+            KichThuoc: item.initialSize,
+            MaSanPham: item.MaSanPham,
+            SoLuong: item.SoLuong,
+        })
+    } catch (error) {
+        console.log(error)
+        return error
+    }
+}
+
+const deleteItemCard = async (item) => {
+    if (!item || !item.MaSanPham) {
+        throw new Error('Sản phẩm không tồn tại');
+    }
+    const userData = await getUserData()
+    try {
+        const db = getDatabase()
+        remove(ref(db, `GioHang/${userData.MaNguoiDung}/${item.MaSanPham}`))
+    } catch (error) {
+        console.log(error)
+        return error
+    }
+}
+
+
+/**
+ * Get cart from database
+ */
+const getCart = async () => {
+    try {
+        const dbRef = ref(getDatabase())
+
+        const cartSnapshot = await get(child(dbRef, 'GioHang/'));
+        const cart = cartSnapshot.val()
+    
+        return cart;
+    } catch(err) {
+        console.log(err);
+        return err;
+    }
+}
+
+export { setCart, getCart, deleteItemCard }
