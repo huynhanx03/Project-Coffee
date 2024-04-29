014ce2438d1b93bd9939e711492ce766fa8c60a9
huynhan03 <huynhnhandanang@gmail.com>
2024-04-29 21:15:05 +0700

    Add statistic

---
+++ Desktop/Coffee/Coffee/ViewModel/AdminVM/Statistic/StatisticPageViewModel.cs
@@ -0,0 +1,41 @@
+﻿using ChartKit.SkiaSharpView;
+using ChartKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChartKit.SkiaSharpView.Painting.Effects;
+using ChartKit.SkiaSharpView.Painting;
+using SkiaSharp;
+using System.Collections.ObjectModel;
+using ChartKit.Defaults;
+using System.Globalization;
+using Coffee.DTOs;
+using System.Windows.Input;
+using Coffee.Services;
+using Coffee.Views.Admin.Table;
+using Microsoft.SqlServer.Server;
+using Newtonsoft.Json.Linq;
+using System.Windows.Markup;
+
+namespace Coffee.ViewModel.AdminVM.Statistic
+{
+    public partial class StatisticViewModel
+    {
+        //Biểu đồ cột x
+        public Axis[] XAxes { get; set; } =
+    {
+        new DateTimeAxis(TimeSpan.FromDays(1), date => date.ToString("dd/MM/yyyy"))
+    };
+        //Biểu đồ cột y
+        public Axis[] YAxes { get; set; }
+            = new Axis[]
+            {
+                new Axis
+                {
+                     Labeler = value => string.Format(new CultureInfo("vi-VN"), "{0:C0}", value)
+                }
+            };
+    }
+}
---
+++ Desktop/Coffee/Coffee/Views/Admin/StatisticPage/StatisticPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using ChartKit.SkiaSharpView;
+using ChartKit;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ChartKit.Defaults;
+using Coffee.DTOs;
+using Coffee.Services;
+using System.Globalization;
+
+namespace Coffee.Views.Admin.StatisticPage
+{
+    /// <summary>
+    /// Interaction logic for StatisticPage.xaml
+    /// </summary>
+    public partial class StatisticPage : Page
+    {
+        #region variable
+        public IEnumerable<ISeries> Series;
+        List<DateTimePoint> dateTimePoints = new List<DateTimePoint>();
+        private ObservableCollection<BillDTO> _BillListchart;
+        public ObservableCollection<BillDTO> BillListchart;
+        #endregion
+        public StatisticPage()
+        {
+            InitializeComponent();
+            Loadthongtinbill();
+            
+        }
+        public async void Loadthongtinbill()
+        {
+            (string label, List<BillDTO> billlist) = await BillService.Ins.getListBill();
+
+            if (billlist != null)
+            {
+                BillListchart = new ObservableCollection<BillDTO>(billlist);
+            }
+            var groupedBills = BillListchart.GroupBy(
+        b => DateTime.ParseExact(b.NgayTao, "HH:mm:ss dd/MM/yyyy", CultureInfo.InvariantCulture).Date,
+        b => Convert.ToInt32(b.TongTien),
+        (date, totals) => new { Date = date, Total = totals.Sum() });
+
+            foreach (var group in groupedBills)
+            {
+                dateTimePoints.Add(new DateTimePoint(group.Date, group.Total));
+            }
+            Series = new ObservableCollection<ISeries>
+           {
+               new LineSeries<DateTimePoint>
+               {
+                   Values = dateTimePoints,
+                   Fill=null
+               }
+           };
+            lineChart.Series = Series;
+        }
+    }
+}
