500d277c49ca66daa21aa482eaefc9498f5cb9e5
Ngo Nam <namngo102003@gmail.com>
2024-05-03 00:13:52 +0700

    order items

--- Mobile/CoffeeApp/components/itemOrder.js
+++ Mobile/CoffeeApp/components/itemOrder.js
@@ -1,5 +1,5 @@
-import { View, Text, Image } from "react-native";
-import React from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import React, { useEffect, useMemo, useState } from "react";
 import {
     heightPercentageToDP as hp,
     widthPercentageToDP as wp,
@@ -8,63 +8,126 @@ import { formatPrice } from "../utils";
 import { Divider } from "react-native-paper";
 import { colors } from "../theme";
 import * as Icons from "react-native-heroicons/outline";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
-const ItemOrder = () => {
+const ItemOrder = (props) => {
+    const navigation = useNavigation()
+    const [quantity, setQuantity] = useState(0);
+    const [isReview, setIsReview] = useState(true);
+    const calQuantity = useMemo(() => {
+        let total = 0;
+        props.order.SanPham.forEach((item) => {
+            total += item.SoLuongGioHang;
+        })
+
+        setQuantity(total);
+    }, [])
+
+    const handleReview = () => {
+        setIsReview(false);
+    }
+
     return (
-        <View className="flex-1 space-y-2 bg-orange-100 rounded-md mt-2">
-            <View className="space-y-5 mx-2">
-                <View className="flex-row justify-between">
-                    <Text className="text-base">Trạng thái</Text>
-                    <Text
-                        className="text-base font-semibold"
-                        style={{ color: colors.text(1) }}
-                    >
-                        Hoàn thành
+        <View className="flex-1">
+            <View className="space-y-3 rounded-md mt-4 bg-white shadow-md">
+                <View className="space-y-5 mx-2">
+                    <View className="flex-row justify-between p-1">
+                        <Text
+                            className="text-base font-bold"
+                            style={{ color: colors.text(1) }}
+                        >
+                            {props.order.MaDonHang}
+                        </Text>
+                        <View className="flex-row justify-end">
+                            <Text className="text-base">Trạng thái: </Text>
+                            <Text
+                                className="text-base font-semibold"
+                                style={{ color: colors.text(1) }}
+                            >
+                                {props.order.TrangThai}
+                            </Text>
+                        </View>
+                    </View>
+
+                    {props.order.SanPham.map((item, index) => {
+                        return (
+                            <View className="flex-row space-x-5">
+                                <Image
+                                    source={require("../assets/images/coffeeDemo.png")}
+                                    style={{
+                                        width: wp(20),
+                                        height: wp(22),
+                                    }}
+                                    className="rounded-lg"
+                                />
+
+                                <View className="flex-1 space-y-2">
+                                    <Text className="text-lg font-semibold">
+                                        {item.TenSanPham}
+                                    </Text>
+                                    <Text className="text-base">
+                                        Size: {item.KichThuoc}
+                                    </Text>
+                                    <View className="flex-row justify-between">
+                                        <Text className="text-base">
+                                            {formatPrice(item.Gia)}
+                                        </Text>
+                                        <Text className="text-base">
+                                            x{item.SoLuongGioHang}
+                                        </Text>
+                                    </View>
+                                </View>
+                            </View>
+                        );
+                    })}
+                </View>
+
+                <Divider className="bg-gray-400 p-[0.2px]" />
+
+                <View className="flex-row items-center justify-between">
+                    <View className='flex-row mx-2'>
+                        <Ionicons
+                            name="fast-food-outline"
+                            size={24}
+                            color={'gray'}
+                        />
+                        <Text className="mx-2 text-base text-gray-500 font-semibold">
+                            {quantity} sản phẩm
+                        </Text>
+                    </View>
+                    <View className="flex-row justify-end mx-2">
+                        <Text className="text-base">Thành tiền: </Text>
+                        <Text
+                            style={{ color: colors.text(1) }}
+                            className="text-base font-semibold"
+                        >
+                            {formatPrice(props.order.ThanhTien)}
+                        </Text>
+                    </View>
+                </View>
+
+                <Divider className="bg-gray-400 p-[0.2px]" />
+
+                <View className="mx-2 flex-row items-center space-x-2">
+                    <Icons.TruckIcon size={30} color={colors.active} />
+                    <Text className="text-base">
+                        Đơn hàng đang trong quá trình xác nhận
                     </Text>
                 </View>
 
-                <View className="flex-row space-x-5">
-                    <Image
-                        source={require("../assets/images/coffeeDemo.png")}
-                        style={{ width: wp(20), height: wp(22) }}
-                        className="rounded-lg"
-                    />
+                <Divider className="bg-gray-400 p-[0.2px]" />
 
-                    <View className="flex-1 space-y-2">
-                        <Text className="text-lg font-semibold">
-                            Cà phê sữa
-                        </Text>
-                        <Text className="text-base">Size: M</Text>
-                        <View className='flex-row justify-between'>
-                            <Text className='text-base'>{formatPrice(200000)}</Text>
-                            <Text className="text-base">x1</Text>
-                        </View>
-                    </View>
+                <View className='flex-row justify-between mx-2 pb-2'>
+                    <TouchableOpacity onPress={handleReview} disabled={!isReview} className='p-3 rounded-lg' style={{backgroundColor: isReview ? colors.active : '#d2d2d2'}}>
+                        <Text className='text-base font-semibold'>Đã nhận được hàng</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => navigation.navigate('Review')} disabled={isReview} className='p-3 rounded-lg' style={{backgroundColor: isReview ? '#d2d2d2' : colors.active}}>
+                        <Text className='text-base font-semibold'>Đánh giá sản phẩm</Text>
+                    </TouchableOpacity>
                 </View>
             </View>
-
-            <Divider className='bg-gray-400 p-[0.2px]'/>
-
-            <View className="flex-row justify-end mx-2">
-                <Text className="text-base">Thành tiền: </Text>
-                <Text
-                    style={{ color: colors.text(1) }}
-                    className="text-base font-semibold"
-                >
-                    {formatPrice(200000)}
-                </Text>
-            </View>
-
-            <Divider className='bg-gray-400 p-[0.2px]'/>
-
-            <View className="mx-2 flex-row items-center space-x-2 pb-2">
-                <Icons.TruckIcon size={30} color={colors.active} />
-                <Text className="text-base">
-                    Đơn hàng đã được giao thành công
-                </Text>
-            </View>
-
-            {/* <Divider className="" style={{ backgroundColor: "#f2f2f2" }} /> */}
         </View>
     );
 };
---
+++ Mobile/CoffeeApp/components/itemOrderList.js
@@ -0,0 +1,32 @@
+import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { getOrder } from "../controller/OrderController";
+import ItemOrder from "./itemOrder";
+
+const ItemOrderList = () => {
+    const [orderInfo, setOrderInfo] = useState(null);
+
+    const handleGetOrder = async () => {
+        try {
+            const info = await getOrder();
+            setOrderInfo(info);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        handleGetOrder();
+    }, []);
+
+    return (
+        <View>
+            {orderInfo &&
+                Object.keys(orderInfo).map((key) => (
+                    <ItemOrder key={key} order={orderInfo[key]} />
+                ))}
+        </View>
+    );
+};
+
+export default ItemOrderList;
