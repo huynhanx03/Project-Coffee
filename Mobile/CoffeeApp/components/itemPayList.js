dd1d4225a6b28f58d6cf41dfb2f50e389000c7b0
Ngo Nam <namngo102003@gmail.com>
2024-05-04 16:24:28 +0700

    animation when entering

--- Mobile/CoffeeApp/components/itemPayList.js
+++ Mobile/CoffeeApp/components/itemPayList.js
@@ -2,19 +2,20 @@ import { View, Text, FlatList } from "react-native";
 import React from "react";
 import { useSelector } from "react-redux";
 import ItemPay from "./itemPay";
+import Animated, { FadeIn, FadeOut } from "react-native-reanimated";
 
 const ItemPayList = () => {
     const cart = useSelector((state) => state.cart.cart);
 
     return (
-        <View>
+        <Animated.View entering={FadeIn} exiting={FadeOut}>
             <FlatList
                 scrollEnabled={false}
                 data={cart}
                 renderItem={({ item }) => <ItemPay item={item} />}
                 keyExtractor={(item) => item.MaSanPham}
             />
-        </View>
+        </Animated.View>
     );
 };
 
