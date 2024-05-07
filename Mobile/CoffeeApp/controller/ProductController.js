0107538833014379ecf0255c03805de0adac0fd6
Ngo Nam <namngo102003@gmail.com>
2024-05-07 10:50:54 +0700

    get products on sale

--- Mobile/CoffeeApp/controller/ProductController.js
+++ Mobile/CoffeeApp/controller/ProductController.js
@@ -35,4 +35,42 @@ const getProducts = async () => {
     }
 }
 
-export { getCategories, getProducts }
+
+/**
+ * @notice Get the products on sale
+ * @returns The products on sale
+ */
+const getProductsSale = async () => {
+    const dbRef = ref(getDatabase());
+
+    try {
+        const productsSaleSnapshot = await get(child(dbRef, 'SanPhamGiamGiaHomNay/'))
+        const productsSale = productsSaleSnapshot.val()
+
+        return productsSale
+    } catch (error) {
+        console.log(error)
+        return error
+    }
+}
+
+const getProductDetail = async () => {
+    const dbRef = ref(getDatabase());
+    const productsSale = await getProductsSale();
+
+    try {
+        const productDetailPromises = Object.values(productsSale).map(async (product) => {
+            const productDetailSnapshot = await get(child(dbRef, `SanPham/${product.MaSanPham}`));
+            const productDetail = productDetailSnapshot.val();
+            return { ...productDetail, PhanTramGiam: product.PhanTramGiam };
+        });
+
+        const productDetailList = await Promise.all(productDetailPromises);
+        return productDetailList;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+};
+
+export { getCategories, getProducts, getProductsSale, getProductDetail }
