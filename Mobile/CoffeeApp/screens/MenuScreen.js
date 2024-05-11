b61ffc6b8fe5a299fa7cdbd4de3956dcbae937f9
Ngo Nam <namngo102003@gmail.com>
2024-05-11 14:14:34 +0700

    feature search products

--- Mobile/CoffeeApp/screens/MenuScreen.js
+++ Mobile/CoffeeApp/screens/MenuScreen.js
@@ -1,5 +1,5 @@
 import { useNavigation } from "@react-navigation/native";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
     Image,
     ScrollView,
@@ -10,31 +10,33 @@ import {
 } from "react-native";
 import Draggable from "react-native-draggable";
 import * as Icons from "react-native-heroicons/outline";
-import Carousel from "react-native-reanimated-carousel";
 import {
     heightPercentageToDP as hp,
     widthPercentageToDP as wp,
 } from "react-native-responsive-screen";
 import Categories from "../components/categories";
 import Item from "../components/item";
-import getDefaultAddress from "../customHooks/getDefaultAddress";
 import { colors } from "../theme";
-import { getCategories, getProducts } from "../controller/ProductController";
-import { useDispatch, useSelector } from "react-redux";
-import { getCart } from "../controller/CartController";
-import { addToCartFromDatabase } from "../redux/slices/cartSlice";
-import { getUserData } from "../controller/StorageController";
+import {
+    getCategories,
+    getProducts,
+    getProductsBestSeller,
+} from "../controller/ProductController";
+import { useSelector } from "react-redux";
 import { SafeAreaView } from "react-native-safe-area-context";
+import LottieView from "lottie-react-native";
 
 const MenuScreen = () => {
     const navigation = useNavigation();
-    const dispatch = useDispatch();
     const [isActive, setIsActive] = useState("Tất cả");
     const [products, setProducts] = useState(null);
     const [categories, setCategories] = useState(null);
-    const [user, setUser] = useState(null);
+    const [proBestSeller, setProBestSeller] = useState([]);
+    const [search, setSearch] = useState("");
+    const [searchResult, setSearchResult] = useState([]);
+    const [searchPage, setSearchPage] = useState(false);
+    const [productFilter, setProductFilter] = useState([])
 
-    const addressData = getDefaultAddress();
 
     const cart = useSelector((state) => state.cart.cart);
 
@@ -61,6 +63,7 @@ const MenuScreen = () => {
                     SoLuong: listProducts[key].SoLuong,
                     LoaiSanPham: listProducts[key].LoaiSanPham,
                     Mota: listProducts[key].Mota,
+                    PhanTramGiam: listProducts[key].PhanTramGiam,
                     Size: {
                         Nho: {
                             TenKichThuoc:
@@ -92,19 +95,29 @@ const MenuScreen = () => {
         setProducts([...allProducts]);
     };
 
-    const getUser = async () => {
-        try {
-            const userData = await getUserData();
-            setUser(userData);
-        } catch (error) {
-            console.log(error);
-        }
+    const handleSearch = (text) => {
+        setSearch(text);
+        
+        const result = products.filter((product) => product.TenSanPham.toLowerCase().includes(text.toLowerCase()));
+        setSearchResult(result);
     };
+
+    const handleGetBestSeller = async () => {
+        const bestSeller = await getProductsBestSeller();
+        setProBestSeller(bestSeller);
+    };
+
+    const handleOnBlur = () => {
+        if (search.length === 0) {
+            setSearchPage(false);
+            setSearchResult([]);
+        }
+    }
 
     useEffect(() => {
         handleGetProducts();
         handleGetCategories();
-        getUser();
+        handleGetBestSeller();
     }, []);
 
     return (
@@ -118,116 +131,180 @@ const MenuScreen = () => {
                         EPSRO
                     </Text>
                     {/* Search bar */}
-                    <View
-                        className="mx-5 bg-white rounded-3xl"
-                    >
+                    <View className="mx-5 bg-white rounded-3xl">
                         <View className="flex-row items-center space-x-3 px-3">
-                            <View className='border-r-[1px] space-x-1 border-gray-400'>
+                            <View className="border-r-[1px] space-x-1 border-gray-400">
                                 <Icons.MagnifyingGlassIcon
                                     size={24}
-                                    color={'gray'}
-                                    className='mr-2'
+                                    color={"gray"}
+                                    className="mr-2"
                                 />
                             </View>
                             <TextInput
                                 keyboardType="web-search"
                                 placeholder="tìm món..."
+                                onChangeText={(text) => handleSearch(text)}
                                 className="text-lg py-4"
-                                style={{lineHeight: wp(5), width: '100%'}}
+                                onFocus={() => setSearchPage(true)}
+                                onBlur={handleOnBlur}
+                                style={{ lineHeight: wp(5), width: "100%" }}
                             />
                         </View>
                     </View>
                 </SafeAreaView>
 
-                {/* categories */}
-                <View
-                    className="rounded-3xl"
-                    style={{ marginTop: wp(-5), backgroundColor: "#f2f2f2" }}
-                >
-                    <View className="mx-2 mt-5">
-                        {categories && (
-                            <Categories
-                                categories={categories}
-                                setIsActive={setIsActive}
-                            />
-                        )}
-                    </View>
-                    {/* card item */}
-                    <View className="mx-5 mt-5 flex-row flex-wrap justify-between">
-                        {products && isActive === "Tất cả"
-                            ? products
-                                  .filter((item) => item.SoLuong > 0)
-                                  .map((product) => {
-                                      return (
-                                          <Item
-                                              product={product}
-                                              key={product.MaSanPham}
-                                          />
-                                      );
-                                  })
-                            : products &&
-                              products
-                                  .filter(
-                                      (item) =>
-                                          item.SoLuong > 0 &&
-                                          item.LoaiSanPham == isActive
-                                  )
-                                  .map((product, index) => {
-                                      return (
-                                          <Item
-                                              product={product}
-                                              key={product.MaSanPham}
-                                          />
-                                      );
-                                  })}
-                    </View>
-                    {/* out of stock */}
-                    {products &&
-                        products.filter((item) => item.SoLuong == 0).length >
-                            0 && (
-                            <View>
-                                {/* <Text className="text-center text-lg font-bold mt-5">Hết hàng</Text> */}
-                                <View className="mt-5 flex-row justify-between items-center mx-5">
-                                    <Text
-                                        style={{
-                                            height: 1,
-                                            borderColor:
-                                                "rgba(59, 29, 12, 0.4)",
-                                            borderWidth: 1,
-                                            width: wp(30),
-                                        }}
-                                    ></Text>
-                                    <Text
-                                        style={{ color: "#8B6122" }}
-                                        className="text-lg font-semibold"
-                                    >
-                                        Hết hàng
-                                    </Text>
-                                    <Text
-                                        style={{
-                                            height: 1,
-                                            borderColor:
-                                                "rgba(59, 29, 12, 0.4)",
-                                            borderWidth: 1,
-                                            width: wp(30),
-                                        }}
-                                    ></Text>
-                                </View>
+                {searchPage ? (
+                    <View
+                        className="rounded-3xl"
+                        style={{
+                            marginTop: wp(-5),
+                            backgroundColor: "#f2f2f2",
+                        }}
+                    >
+                        {
+                            searchResult && searchResult.length > 0 ? (
                                 <View className="mx-5 mt-5 flex-row flex-wrap justify-between">
-                                    {products
-                                        .filter((item) => item.SoLuong == 0)
-                                        .map((product, index) => {
+                                    {
+                                        searchResult.map((product) => {
                                             return (
                                                 <Item
                                                     product={product}
                                                     key={product.MaSanPham}
+                                                    isSale={
+                                                        product.PhanTramGiam > 0
+                                                    }
+                                                    isBestSeller={proBestSeller.includes(
+                                                        product.MaSanPham
+                                                    )}
                                                 />
                                             );
-                                        })}
+                                        })
+                                    }
                                 </View>
-                            </View>
-                        )}
-                </View>
+                            ) : (
+                                <LottieView
+                                    source={require("../assets/lottie/Search.json")}
+                                    autoPlay
+                                    loop
+                                    speed={0.7}
+                                    style={{
+                                        height: hp(50),
+                                        width: wp(80),
+                                        alignSelf: "center",
+                                        marginTop: 50,
+                                        justifyContent: "center",
+                                    }}
+                                />
+                            )
+                        }
+                    </View>
+                ) : (
+                    <View
+                        className="rounded-3xl"
+                        style={{
+                            marginTop: wp(-5),
+                            backgroundColor: "#f2f2f2",
+                        }}
+                    >
+                        <View className="mx-2 mt-5">
+                            {categories && (
+                                <Categories
+                                    categories={categories}
+                                    setIsActive={setIsActive}
+                                />
+                            )}
+                        </View>
+                        {/* card item */}
+                        <View className="mx-5 mt-5 flex-row flex-wrap justify-between">
+                            {products && isActive === "Tất cả"
+                                ? products
+                                      .filter((item) => item.SoLuong > 0)
+                                      .map((product) => {
+                                          return (
+                                              <Item
+                                                  product={product}
+                                                  key={product.MaSanPham}
+                                                  isSale={
+                                                      product.PhanTramGiam > 0
+                                                  }
+                                                  isBestSeller={proBestSeller.includes(
+                                                      product.MaSanPham
+                                                  )}
+                                              />
+                                          );
+                                      })
+                                : products &&
+                                  products
+                                      .filter(
+                                          (item) =>
+                                              item.SoLuong > 0 &&
+                                              item.LoaiSanPham == isActive
+                                      )
+                                      .map((product, index) => {
+                                          return (
+                                              <Item
+                                                  product={product}
+                                                  key={product.MaSanPham}
+                                                  isSale={
+                                                      product.PhanTramGiam > 0
+                                                  }
+                                                  isBestSeller={proBestSeller.includes(
+                                                      product.MaSanPham
+                                                  )}
+                                              />
+                                          );
+                                      })}
+                        </View>
+                        {/* out of stock */}
+                        {products &&
+                            products.filter((item) => item.SoLuong == 0)
+                                .length > 0 && (
+                                <View>
+                                    {/* <Text className="text-center text-lg font-bold mt-5">Hết hàng</Text> */}
+                                    <View className="mt-5 flex-row justify-between items-center mx-5">
+                                        <Text
+                                            style={{
+                                                height: 1,
+                                                borderColor:
+                                                    "rgba(59, 29, 12, 0.4)",
+                                                borderWidth: 1,
+                                                width: wp(30),
+                                            }}
+                                        ></Text>
+                                        <Text
+                                            style={{ color: "#8B6122" }}
+                                            className="text-lg font-semibold"
+                                        >
+                                            Hết hàng
+                                        </Text>
+                                        <Text
+                                            style={{
+                                                height: 1,
+                                                borderColor:
+                                                    "rgba(59, 29, 12, 0.4)",
+                                                borderWidth: 1,
+                                                width: wp(30),
+                                            }}
+                                        ></Text>
+                                    </View>
+                                    <View className="mx-5 mt-5 flex-row flex-wrap justify-between">
+                                        {products
+                                            .filter((item) => item.SoLuong == 0)
+                                            .map((product, index) => {
+                                                return (
+                                                    <Item
+                                                        product={product}
+                                                        key={product.MaSanPham}
+                                                    />
+                                                );
+                                            })}
+                                    </View>
+                                </View>
+                            )}
+                    </View>
+                )}
+
+                {/* categories */}
             </ScrollView>
 
             {/* cart */}
