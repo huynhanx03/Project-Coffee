a313a521d92e01d7642b14a07efa337c476035bc
Ngo Nam <namngo102003@gmail.com>
2024-05-13 11:49:45 +0700

    update ui change dob and sex

--- Mobile/CoffeeApp/screens/ChangeInfoScreen.js
+++ Mobile/CoffeeApp/screens/ChangeInfoScreen.js
@@ -9,10 +9,20 @@ import * as Icons from "react-native-heroicons/outline";
 import { colors } from "../theme";
 import { updateInfo } from "../controller/ChangeInfoController";
 import ShowToast from "../components/toast";
+import DropDownPicker from "react-native-dropdown-picker";
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { Ionicons } from '@expo/vector-icons';
 
 const ChangeInfoScreen = ({route}) => {
     const {label, content} = route.params;
     const [newInfo, setNewInfo] = useState('');
+    const [date, setDate] = useState(new Date());
+    const [open, setOpen] = useState(false)
+    const [items, setItems] = useState([
+        {label: 'Nam', value: 'Nam', icon: () => <Ionicons name="male" size={24} color="black" />},
+        {label: 'Nữ', value: 'Nữ', icon: () => <Ionicons name="female" size={24} color="black" />},
+        {label: 'Khác', value: 'Khác', icon: () => <Ionicons name="male-female" size={24} color="black" />}
+    ])
     const navigation = useNavigation();
     let type = '';
 
@@ -33,10 +43,15 @@ const ChangeInfoScreen = ({route}) => {
         }
     }
 
+    const handleChangeDate = (e, selectedDate) => {
+        const currentDate = selectedDate || date;
+        setNewInfo(currentDate.toLocaleDateString('vi-VN'))
+    }
+
     return (
-        <View className='flex-1'>
-            <View>
-                <ScrollView showsVerticalScrollIndicator={false}>
+        <View className="flex-1">
+            <ScrollView>
+                <View>
                     {/* avatar */}
                     <View style={{ height: hp(25) }} className="bg-yellow-950">
                         <View className="justify-center items-center mt-20 mx-5">
@@ -65,7 +80,10 @@ const ChangeInfoScreen = ({route}) => {
                     </View>
 
                     {/* edit password */}
-                    <View style={{backgroundColor: '#f2f2f2'}} className='-mt-5 rounded-3xl'>
+                    <View
+                        style={{ backgroundColor: "#f2f2f2" }}
+                        className="-mt-5 rounded-3xl"
+                    >
                         <View className="mx-5">
                             <View className="space-y-1 mt-5">
                                 <Text
@@ -78,7 +96,9 @@ const ChangeInfoScreen = ({route}) => {
                                     className="border rounded-lg"
                                     style={{ borderColor: "#9d9d9d" }}
                                 >
-                                    <Text className='text-base p-3'>{content}</Text>
+                                    <Text className="text-base p-3">
+                                        {content}
+                                    </Text>
                                 </View>
                             </View>
                             <View className="space-y-1 mt-5">
@@ -88,29 +108,72 @@ const ChangeInfoScreen = ({route}) => {
                                 >
                                     Thông tin mới
                                 </Text>
-                                <View
-                                    className="border rounded-lg"
-                                    style={{ borderColor: "#9d9d9d" }}
-                                >
-                                    <TextInput
-                                        value={newInfo}
-                                        onChangeText={(text) => setNewInfo(text)}
-                                        className="p-3 text-base"
-                                        placeholder="Thông tin mới"
-                                    />
-                                </View>
+                                {type === "GioiTinh" ? (
+                                    <View
+                                        className="border rounded-lg"
+                                        style={{ borderColor: "#9d9d9d" }}
+                                    >
+                                        <DropDownPicker
+                                            placeholder="Chọn giới tính"
+                                            open={open}
+                                            value={newInfo}
+                                            items={items}
+                                            setOpen={setOpen}
+                                            setValue={setNewInfo}
+                                            setItems={setItems}
+                                            textStyle={{ fontSize: wp(4) }}
+                                            labelStyle={{ fontSize: wp(4) }}
+                                        />
+                                    </View>
+                                ) : type === "NgaySinh" ? (
+                                    <View>
+                                        <DateTimePicker
+                                            value={date}
+                                            mode="date"
+                                            display="inline"
+                                            onChange={handleChangeDate}
+                                            locale="vi-VN"
+                                            maximumDate={new Date()}
+                                        />
+                                    </View>
+                                ) : (
+                                    <View
+                                        className="border rounded-lg"
+                                        style={{ borderColor: "#9d9d9d" }}
+                                    >
+                                        <TextInput
+                                            value={newInfo}
+                                            onChangeText={(text) =>
+                                                setNewInfo(text)
+                                            }
+                                            className="p-3 text-base"
+                                            placeholder="Thông tin mới"
+                                        />
+                                    </View>
+                                )}
                             </View>
                         </View>
                     </View>
 
                     {/* button */}
-                    <View className="mt-10 mx-5">
-                        <TouchableOpacity onPress={handleUpdate} className='rounded-md' style={{backgroundColor: colors.primary}}>
-                            <Text className='py-3 text-center font-semibold text-white text-xl'>Cập nhật</Text>
+                    <View
+                        className="mx-5"
+                        style={{
+                            marginTop: type === "GioiTinh" ? wp(30) : wp(20),
+                        }}
+                    >
+                        <TouchableOpacity
+                            onPress={handleUpdate}
+                            className="rounded-md"
+                            style={{ backgroundColor: colors.primary }}
+                        >
+                            <Text className="py-3 text-center font-semibold text-white text-xl">
+                                Cập nhật
+                            </Text>
                         </TouchableOpacity>
                     </View>
-                </ScrollView>
-            </View>
+                </View>
+            </ScrollView>
         </View>
     );
 };
